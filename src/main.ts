b883ee50ec3ab5e73eec01179caaab2016cb2529
Maximilian Rüsch <78490564+maximilianruesch@users.noreply.github.com>
2022-10-20 21:30:18 +0200

    Add nested evaluation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import * as core from '@actions/core'
 
+function evaluateCondition({ cond, ifTrue, ifFalse }:
+  { cond: string, ifTrue: string|Object, ifFalse: string|Object }): string {
+  const evaluatedIfTrue: string = typeof ifTrue === Object ? evaluateCondition({
+    cond: ifTrue.cond,
+    ifTrue: ifTrue.ifTrue,
+    ifFalse: ifTrue.ifFalse,
+  }) : ifTrue
+
+  const evaluatedIfFalse: string = typeof ifFalse === Object ? evaluateCondition({
+    cond: ifFalse.cond,
+    ifTrue: ifFalse.ifTrue,
+    ifFalse: ifFalse.ifFalse,
+  }) : ifFalse
+  
+  return cond === 'true' ? evaluatedIfTrue : evaluatedIfFalse
+}
+
 async function run(): Promise<void> {
   try {
-    const cond: string = core.getInput('cond', {required: true})
-    const ifTrue: string = core.getInput('if_true')
-    const ifFalse: string = core.getInput('if_false')
-    core.setOutput('value', cond === 'true' ? ifTrue : ifFalse)
+    const cond: string = core.getInput('cond', { required: true })
+    const ifTrue: string|Object = core.getInput('if_true')
+    const ifFalse: string|Object = core.getInput('if_false')
+    core.setOutput('value', evaluateCondition({ cond, ifTrue, ifFalse }))
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
