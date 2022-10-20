1a1f8f0210d226b9dd173a382bbcbd18302f94f7
Maximilian Rüsch <maximilian.ruesch@pickware.de>
2022-10-20 22:15:32 +0200

    Apply linting rules

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,42 @@
 import * as core from '@actions/core'
 
-function evaluateCondition({ cond, ifTrue, ifFalse }:
-  { cond: string, ifTrue: string|Object, ifFalse: string|Object }): string {
-  const evaluatedIfTrue: string = typeof ifTrue === Object ? evaluateCondition({
-    cond: ifTrue.cond,
-    ifTrue: ifTrue.ifTrue,
-    ifFalse: ifTrue.ifFalse,
-  }) : ifTrue
+interface NestedConditionType {
+  cond: string
+  ifTrue: string | NestedConditionType
+  ifFalse: string | NestedConditionType
+}
+function evaluateCondition({
+  cond,
+  ifTrue,
+  ifFalse
+}: NestedConditionType): string {
+  const evaluatedIfTrue: string =
+    typeof ifTrue === 'object'
+      ? evaluateCondition({
+          cond: ifTrue.cond,
+          ifTrue: ifTrue.ifTrue,
+          ifFalse: ifTrue.ifFalse
+        })
+      : ifTrue
 
-  const evaluatedIfFalse: string = typeof ifFalse === Object ? evaluateCondition({
-    cond: ifFalse.cond,
-    ifTrue: ifFalse.ifTrue,
-    ifFalse: ifFalse.ifFalse,
-  }) : ifFalse
-  
+  const evaluatedIfFalse: string =
+    typeof ifFalse === 'object'
+      ? evaluateCondition({
+          cond: ifFalse.cond,
+          ifTrue: ifFalse.ifTrue,
+          ifFalse: ifFalse.ifFalse
+        })
+      : ifFalse
+
   return cond === 'true' ? evaluatedIfTrue : evaluatedIfFalse
 }
 
 async function run(): Promise<void> {
   try {
-    const cond: string = core.getInput('cond', { required: true })
-    const ifTrue: string|Object = core.getInput('if_true')
-    const ifFalse: string|Object = core.getInput('if_false')
-    core.setOutput('value', evaluateCondition({ cond, ifTrue, ifFalse }))
+    const cond: string = core.getInput('cond', {required: true})
+    const ifTrue: string | NestedConditionType = core.getInput('if_true')
+    const ifFalse: string | NestedConditionType = core.getInput('if_false')
+    core.setOutput('value', evaluateCondition({cond, ifTrue, ifFalse}))
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
